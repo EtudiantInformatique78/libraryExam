5fdd6ba3361027dd06f445c9c71ffdf74c48ed76
Khaarn <shrike3emecompagnie@gmail.com>
2023-11-09 15:06:33 +0100

    Ajout de la fonction showBooks 2

--- main.c
+++ main.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
-typedef struct Livres
+typedef struct Books
 {
-    char Title[20];
-    char Autor;
-    int ancien;
+    char title[20];
+    char autor[20];
+    int publicationYear;
+    bool state;
+    int remainingDays;
 
-}Livres;
+}Books;
+
+void showBooks(const struct Books *book)
+{
+    printf("Title : %s\n", book->title);
+    printf("Autor : %s\n", book->autor);
+    printf("Publication Year : %d\n", book->publicationYear);
+
+    if(state == 1)
+    {
+        printf("The book is not available for now.\n");
+        printf("Remaining days : %d\n", book->remainingDays);
+    }
+    else
+    {
+        printf("The book is available to borrow.")
+        printf("You can borrow it now :\n");
+    }
+}
 
 int main(int argc, char** argv)
 {
-
 
     return 0;
 }
